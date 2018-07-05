9009aefe112813bf5dafecc4ac646d475de62491
mohamedidris45 <mohamedidris45@yahoo.com>
2018-07-05 21:01:54 +0530

    added Nginx

---
+++ Nginx/Nginx.md
@@ -0,0 +1,33 @@
+This simple script is used to install nginx in .rpm (Redhat, Fedora, CentOS) Distros 
+
+CAUTION :
+
+	* Don't use this script with other then .rpm distros
+	* This script tested in Centos 7 
+
+STEPS :
+
+	1. Download script in your Cloud Linux by following ways, 
+
+		* yum install git
+		* git clone https://github.com/MohamedIdris45/Basic-Setup.git
+		* cd Basic-Setup/Nginx
+		* bash nginx.sh (root permission)
+	
+	2. This script install nginx latest version
+
+	3. To Test Nginx webserver working or not by 
+		
+		* typing your ip address in web-browser
+		* get your ip address ip typing (ip address in your terminal)
+
+	4. Next move you web-site folder to /var/www/your website name/html/ 
+
+	5. replace default index.html with your index.html
+
+	7. Now check you web-site is working by ip address in web-browser
+
+	8. Feedback or Report @ mohamedidris45@yahoo.com
+
+
+
---
+++ Nginx/Nginx.sh
@@ -0,0 +1,112 @@
+#!/bin/bash 
+
+# AUTHOR : MOHAMEDIDRIS
+# VERSION : 1.1v
+# CONTACT : mohamedidris45@yahoo.com
+
+
+read -p "Have you read the manual [y/n] : " yes
+
+x=$(echo "y")
+
+if [ "$yes" == "$x" ];then
+
+
+	echo "#######################################################################"
+	echo "################  INSTALLING NGINX IN CENTOS  #########################"
+	echo "#######################################################################"
+
+	yum install epel-release -y
+	yum install firewalld -y
+	yum install nginx -y
+
+	systemctl start firewalld
+	systemctl start nginx
+	systemctl enable firewalld
+	systemctl enable nginx
+
+	firewall-cmd --permanent --add-port=80/tcp
+	firewall-cmd --permanent --add-service=http
+	firewall-cmd --reload
+
+	cp -rvf /etc/nginx /root/
+
+	mkdir /etc/nginx/sites-available
+	mkdir /etc/nginx/sites-enabled
+	
+	while :
+	do
+		echo ""
+		echo "#############  ENTER YOU WEBSITE NAME WITHOUT (WWW & .COM)  ##############"
+		echo ""
+
+		read -p "Enter your Website name without [www] & [.com]: " web
+
+		tes="$web"
+
+		if [ -n "$tes" ]; then
+
+			sed -i -e '36,57 d' /etc/nginx/nginx.conf
+			sed -i -e '36 a include /etc/nginx/sites-enabled/*.conf;'  /etc/nginx/nginx.conf
+
+			touch /etc/nginx/sites-available/$web.conf
+
+
+			mkdir -p /var/www/$web.com/html
+
+			semanage fcontext -a -t httpd_sys_content_t "web(/.*)?"
+
+			restorecon -R -v /var/www/
+
+			echo "server {
+    				listen  80;
+
+    				server_name $web.com www.$web.com;
+
+    				location / {
+        				root  /var/www/$web.com/html;
+        				index  index.html index.htm;
+        				try_files \$uri \$uri/ =404;
+    				}	
+
+    				error_page  500 502 503 504  /50x.html;
+    				location = /50x.html {
+        				root  /usr/share/nginx/html;
+    				}
+			}" >> /etc/nginx/sites-available/$web.conf
+
+			ln -s /etc/nginx/sites-available/$web.conf /etc/nginx/sites-enabled/
+
+			touch /var/www/$web.com/html/index.html
+
+			echo "<h1>WELCOME TO NGINX ,, ITS WORKING  
+			###### FEEDBACK @ mohamedidris45@yahoo.com ####### </h1>" >> /var/www/$web.com/html/index.html
+
+			systemctl restart nginx
+
+			echo "###############################################################"
+			echo "###############  KINDLY CHECK NGINX IS WORKING ################"
+			echo "############### YOUR IP ADDRESS IN WEB BROWSER ################"
+			echo "###############################################################"
+
+			break
+
+		else
+			
+			echo ""
+
+		fi
+	
+	done
+
+else
+
+	echo "##########  KINDLY READ THE NGINX.md  ##############"
+        exit 2
+fi
+
+
+
+
+
+
